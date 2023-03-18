d0402461da118eb067dc9cb0bafd691f0924fd16
Daystar <er.sagarkoli@gmail.com>
2023-03-18 19:22:39 +0530

    fixed padding issues

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -90,8 +90,8 @@ function Featured() {
         </div>
       </section>
       {/* featured cta */}
-      <div className="featured-cta flex justify-center text-left items-center md1000:bg-none md1000:bg-[#ff0336] ">
-        <div className="container page-padding ">
+      <div className="featured-cta flex justify-center text-left items-center md1000:bg-none md1000:bg-[#ff0336] min620:mb-[12.5rem] ">
+        <div className="container page-padding min620:">
           <div className="flex items-center md1000:flex-col md1000:gap-12  md1000:text-center">
             <h2 className="text-white font-bold text-[3.7rem] md1000:text-[3rem] max-w-6xl px-7 leading-[1.2] min450:text-[2.4rem] ">
               We Are Always Providing Best Fitness Service For You
